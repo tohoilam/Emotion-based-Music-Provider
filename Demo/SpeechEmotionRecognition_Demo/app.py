4162db4bea59f4e1327aa4624da163b61a8a011c
tohoilam <61353084+tohoilam@users.noreply.github.com>
2022-12-25 01:57:24 +0800

    Web Demo Layout

--- Demo/SpeechEmotionRecognition_Demo/app.py
+++ Demo/SpeechEmotionRecognition_Demo/app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-modelName = ""
+modelName = "12-24 22h45m59s (Experiment 13) CNN Model B (200 Epochs) (IEMOCAP EmoDB) (Data Aug 3A) (5 Emotions with Merge and Split 4 Ignore 2) (00001 lr 0001 decay)"
 modelDir = os.path.join(os.getcwd(), "models", modelName)
 model = tf.keras.models.load_model(modelDir)
 
@@ -15,7 +15,10 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-  float_features = [float(x) for x in request.form.values()]
-  features = [np.array(float_features)]
+  testing1 = request.form.get('testing1')
   
+  return render_template('index.html', prediction_text=testing1)
   # self.y_pred = np.argmax(model.predict(self.x_test), axis=1)
+
+if __name__ == "__main__":
+  app.run()
