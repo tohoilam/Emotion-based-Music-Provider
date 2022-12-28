94434c94d2d8b5539d49169d926904ab59416e9f
tohoilam <61353084+tohoilam@users.noreply.github.com>
2022-12-29 03:21:08 +0800

    Audio data pass to backend

--- Demo/SpeechEmotionRecognition_Demo/app.py
+++ Demo/SpeechEmotionRecognition_Demo/app.py
@@ -1,13 +1,24 @@
 import os
+import json
 import numpy as np
 import tensorflow as tf
 from flask import Flask, request, render_template
 
+from DataProcessing import DataProcessing
+
+
 app = Flask(__name__)
 
 modelName = "12-24 22h45m59s (Experiment 13) CNN Model B (200 Epochs) (IEMOCAP EmoDB) (Data Aug 3A) (5 Emotions with Merge and Split 4 Ignore 2) (00001 lr 0001 decay)"
+labelsToInclude = ['Anger', 'Frustration', 'Happiness', 'Neutral', 'Sadness']
+splitDuration = 4
+ignoreDuration = 2
+
+# Load Model
+print('Loading Model...')
 modelDir = os.path.join(os.getcwd(), "models", modelName)
 model = tf.keras.models.load_model(modelDir)
+print('Model Loading Completed!\n')
 
 @app.route('/')
 def home():
@@ -15,9 +26,18 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-  testing1 = request.form.get('testing1')
+  data = request.get_json()
+  # print(data['sampling_rates'])
+  # print(data['audio_data'][0])
+  x_test = data['audio_data'][0]
   
-  return render_template('index.html', prediction_text=testing1)
+  # Load Data
+  dataModel = DataProcessing(labelsToInclude=labelsToInclude, splitDuration=splitDuration, ignoreDuration=ignoreDuration)
+  dataModel.loadAndExtractTestData()
+  dataModel.processData()
+  
+  
+  return render_template('index.html', prediction_text='successful')
   # self.y_pred = np.argmax(model.predict(self.x_test), axis=1)
 
 if __name__ == "__main__":
--- Demo/SpeechEmotionRecognition_Demo/static/script.js
+++ Demo/SpeechEmotionRecognition_Demo/static/script.js
@@ -1,3 +1,5 @@
+const sampleRate = 16000;
+
 jQuery(document).ready(function () {
 	var $ = jQuery;
 	var myRecorder = {
@@ -56,6 +58,48 @@ jQuery(document).ready(function () {
 
 						// Append to the list
 						listObject.append(holderObject);
+
+						if (true) {
+							// Bug: Can't load audio data after puttint it out to 
+							try {
+								context = new AudioContext();
+								var audioBuffer = null;
+
+								try {
+									var request = new XMLHttpRequest();
+									request.open('GET', url, true);
+									request.responseType = 'arraybuffer';
+
+									// Decode asynchronously
+									request.onload = () => {
+										context.decodeAudioData(request.response,
+											(buffer) => {
+												audioBuffer = buffer;
+												// console.log("Duration: " + audioBuffer.duration)
+												// console.log("Length: " + audioBuffer.length)
+												// console.log("Channels: " + audioBuffer.numberOfChannels)
+												// console.log("SR: " + audioBuffer.sampleRate)
+
+												listAudioData.push(audioBuffer.getChannelData(0))
+												listSR.push(audioBuffer.sampleRate)
+												
+											},
+											(err) => {
+												console.error(`Error with decoding audio data: ${err.err}`)
+											}
+										);
+									}
+									request.send();
+								}
+								catch(e) {
+									alert('Request to retrieve audio data failed')
+									console.log(e.err)
+								}
+							}
+							catch(e) {
+								alert('Web Audio API is not supported in this browser');
+							}
+							}
 					});
 				}
 			}
@@ -71,7 +115,6 @@ jQuery(document).ready(function () {
 	$('[data-role="controls"] > button').click(function () {
 		// Initialize the recorder
 		myRecorder.init();
-		listAudioData.push('hihihih');
 
 		// Get the button state 
 		var buttonState = !!$(this).attr('data-recording');
@@ -89,157 +132,42 @@ jQuery(document).ready(function () {
 
 	// Console Log Audio Data List
 	$('[data-role="predict_emotion_button"]').click(() => {
+		var url = "http://127.0.0.1:5000/predict"
+		
 		console.log(listAudioData)
 		console.log(listSR)
+		const data = {audio_data: listAudioData, sampling_rates: listSR}
+		const dataJson = JSON.stringify(data)
+		$.ajax(url, { type: 'POST',
+			dataType: 'json',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: dataJson,
+			contentType: 'application/json'
+		})
+		.done(() => {
+			console.log('Successful');
+		})
+		.fail(() => {
+			console.log('Failed');
+		});
+		// try {
+		// 	var request = new XMLHttpRequest();
+		// 	request.open('POST', url, true);
+		// 	request.responseType = 'text';
+		// 	request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+
+		// 	// Decode asynchronously
+		// 	request.onload = () => {
+		// 		console.log('Successful')
+		// 	}
+		// 	request.send(dataJson);
+		// }
+		// catch(e) {
+		// 	alert('Request to retrieve audio data failed')
+		// 	console.log(e.err)
+		// }
 	})
 });
-
-// jQuery(document).ready(function () {
-
-// 	listAudioData = []
-// 	listSR = []
-
-// 	var $ = jQuery;
-// 	var myRecorder = {
-// 		objects: {
-// 		  	context: null,
-// 		  	stream: null,
-// 		  	recorder: null
-// 		},
-// 		init: function () {
-// 		  if (null === myRecorder.objects.context) {
-// 			myRecorder.objects.context = new (
-// 			  window.AudioContext || window.webkitAudioContext
-// 			);
-// 		  }
-// 		},
-// 		start: function () {
-// 		  var options = {audio: true, video: false};
-// 		  navigator.mediaDevices.getUserMedia(options).then(function (stream) {
-// 			myRecorder.objects.stream = stream;
-// 			myRecorder.objects.recorder = new Recorder(
-// 			  myRecorder.objects.context.createMediaStreamSource(stream),
-// 			  {numChannels: 1}
-// 			);
-// 			myRecorder.objects.recorder.record();
-// 		  }).catch(function (err) {});
-// 		},
-// 		stop: function (listObject, listAudioData, listSR) {
-// 		  if (null !== myRecorder.objects.stream) {
-// 			myRecorder.objects.stream.getAudioTracks()[0].stop();
-// 		  }
-// 		  if (null !== myRecorder.objects.recorder) {
-// 			myRecorder.objects.recorder.stop();
-// 			var url = ""
-
-// 			// Validate object
-// 			if (null !== listObject
-// 				&& 'object' === typeof listObject
-// 				&& listObject.length > 0) {
-// 			  // Export the WAV file
-// 			  myRecorder.objects.recorder.exportWAV(function(blob) {
-// 				var url = (window.URL || window.webkitURL)
-// 						.createObjectURL(blob);
-
-// 				// Prepare the playback
-// 				var audioObject = $('<audio controls></audio>')
-// 						.attr('src', url);
-
-// 				// Prepare the download link
-// 				var downloadObject = $('<a>&#9660;</a>')
-// 						.attr('href', url)
-// 						.attr('download', new Date().toUTCString() + '.wav');
-
-// 				// Wrap everything in a row
-// 				var holderObject = $('<div class="row"></div>')
-// 						.append(audioObject)
-// 						.append(downloadObject)
-// 						.append(getAudioObject);
-
-
-// 				// Append to the list of audio to predict
-// 				listObject.append(holderObject);
-
-// 				console.log("print!!")
-// 			  });
-// 			}
-
-// 			if (true) {
-// 			  // Bug: Can't load audio data after puttint it out to 
-// 			  try {
-// 				console.log('In try')
-// 				console.log(url)
-
-// 				context = new AudioContext();
-// 				var audioBuffer = null;
-
-// 				try {
-// 				  var request = new XMLHttpRequest();
-// 				  request.open('GET', url, true);
-// 				  request.responseType = 'arraybuffer';
-
-// 				  // Decode asynchronously
-// 				  request.onload = () => {
-// 					context.decodeAudioData(request.response,
-// 					  (buffer) => {
-// 						print('on load')
-// 						audioBuffer = buffer;
-// 						console.log("Duration: " + audioBuffer.duration)
-// 						console.log("Length: " + audioBuffer.length)
-// 						console.log("Channels: " + audioBuffer.numberOfChannels)
-// 						console.log("SR: " + audioBuffer.sampleRate)
-// 						console.log(audioBuffer.getChannelData(0))
-// 						console.log(audioBuffer)
-
-// 						listAudioData.push(audioBuffer.getChannelData(0))
-// 						listSR.push(audioBuffer.sampleRate)
-						
-// 					  },
-// 					  (err) => {
-// 						console.error(`Error with decoding audio data: ${err.err}`)
-// 					  } 
-// 					);
-// 				  }
-// 				  request.send();
-// 				}
-// 				catch(e) {
-// 				  // alert('Request to retrieve audio data failed')
-// 				  console.log(e.err)
-// 				}
-// 			  }
-// 			  catch(e) {
-// 				alert('Web Audio API is not supported in this browser');
-// 			  }
-// 			}
-// 		  }
-// 		}
-// 	  };
-
-// 	  // Prepare the recordings list
-// 	  var listObject = $('[data-role="recordings"]');
-
-// 	  // Prepare the record button
-// 	  $('[data-role="controls"] > button').click(function () {
-// 		// Initialize the recorder
-// 		myRecorder.init();
-// 		listAudioData.push('hihihih');
-
-// 		// Get the button state 
-// 		var buttonState = !!$(this).attr('data-recording');
-
-// 		// Toggle
-// 		if (!buttonState) {
-// 		  $(this).attr('data-recording', 'true');
-// 		  myRecorder.start();
-// 		} else {
-// 		  $(this).attr('data-recording', '');
-// 		  myRecorder.stop(listObject, listAudioData, listSR);
-// 		}
-// 	  });
-
-// 	  // Console Log Audio Data LIst
-// 	  $('[data-role="predict_emotion_button"]').click(() => {
-// 		console.log(listAudioData)
-// 		console.log(listSR)
-// 	  })
-// 	});
